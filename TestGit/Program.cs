124ba7b866fd396af407da16fad00981d8740889
Test git <minhtq@gameestudio.com>
2025-03-01 22:47:35 +0700

    Đạt ngu vcl 2

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Đạt nguuuuuuu");
 Console.WriteLine("Đạt nguuuuuuu VCL");
+Console.WriteLine("Đạt nguuuuuuu VCL 2");
